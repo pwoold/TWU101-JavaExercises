4bf7171a8725c3422e7a0fd274b10a4e04ec1ba0
Phoebe Wooldridge <pwooldri@c02vt3zuhtd5.local>
2018-03-07 15:57:46 -0600

    committing initial exercise 5

--- src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise5/RectangleAverager.java
@@ -3,6 +3,12 @@ package com.thoughtworks.tw101.exercises.exercise5;
 public class RectangleAverager {
 
     public float averageArea(Rectangle[] rectangles) {
-        return 0;
+        int sum = 0;
+        for(int i=0; i<rectangles.length; i++){
+            sum+=rectangles[i].area();
+        }
+
+        float averageArea = sum/rectangles.length;
+        return averageArea;
     }
 }
