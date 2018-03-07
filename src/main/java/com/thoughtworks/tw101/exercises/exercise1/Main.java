44d989626ae887bd2fc880ee15d7cfe92ca06d4a
Phoebe Wooldridge <pwooldri@c02vt3zuhtd5.local>
2018-03-07 11:57:10 -0600

    put code from exercise 1 into its own method; submitting exercise 2

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -4,6 +4,10 @@ package com.thoughtworks.tw101.exercises.exercise1;
 
 public class Main {
     public static void main(String[] args) {
+        printOddNumbers1To100AndSum();
+    }
+
+    private static void printOddNumbers1To100AndSum() {
         int sum = 0;
         for(int i=0; i<100; i++){
             if(i%2==1){
--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,14 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+
+    private int count;
+
+    void increment() {
+        count++;
+    }
+
+    void total() {
+        System.out.println(count);
+    }
 }
