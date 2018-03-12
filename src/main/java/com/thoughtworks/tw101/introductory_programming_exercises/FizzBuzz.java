d160bd5c8d1936f25ae0ea750f3c9788f8cb186f
Phoebe Wooldridge <pwooldri@c02vt3zuhtd5.local>
2018-03-12 17:00:38 -0500

    committing initial fizzbuzz exercise

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/FizzBuzz.java
@@ -32,6 +32,29 @@ public class FizzBuzz {
     }
 
     private static void fizzBuzz() {
+        for(int i=0; i<100; i++){
+            System.out.println(checkValue(i+1));
+        }
+    }
 
+    private static Boolean check3(int i){
+        return i%3==0;
+    }
+
+    private static Boolean check5(int i){
+        return i%5==0;
+    }
+
+    private static String checkValue(int i){
+        if(check3(i) && check5(i)){
+            return "FizzBuzz";
+        }
+        if(check3(i)){
+            return "Fizz";
+        }
+        if(check5(i)) {
+            return "Buzz";
+        }
+        return Integer.toString(i);
     }
 }
