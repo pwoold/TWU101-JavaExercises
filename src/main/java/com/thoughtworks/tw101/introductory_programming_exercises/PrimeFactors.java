cdffeb9b0867370ca4434b638787b03606894dd2
Phoebe Wooldridge <pwooldri@c02vt3zuhtd5.local>
2018-03-13 10:07:14 -0500

    committing prime factors exercise

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -6,14 +6,40 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
-import java.util.List;
+import java.util.*;
 
 public class PrimeFactors {
     public static void main(String[] args) {
         List<Integer> primeFactors = generate(30);
+        sortAndPrintList(primeFactors);
     }
 
     private static List<Integer> generate(int n) {
-        return null;
+
+        List<Integer> primeFactors = new ArrayList<>();
+
+        for (int i=2; i<n; i++) {
+            if (n%i==0) {
+                if (isPrime(i)) {
+                    primeFactors.add(i);
+                }
+            }
+        }
+
+        return primeFactors;
+    }
+
+    private static boolean isPrime(int n) {
+        for (int i=2; i<n/2+1; i++) {
+            if (n%i==0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void sortAndPrintList(List<Integer> list) {
+        Collections.sort(list);
+        System.out.println(list);
     }
 }
